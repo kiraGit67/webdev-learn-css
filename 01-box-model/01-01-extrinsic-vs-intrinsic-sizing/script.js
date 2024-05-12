ecb06422cf8b0f03fb6bf413d801e0fafe63b82a
kiraGit67 <kirsten@sais.de>
2024-05-12 23:11:32 +0200

    adds eventListener to intrinsicSwitch

--- 01-box-model/01-01-extrinsic-vs-intrinsic-sizing/script.js
+++ 01-box-model/01-01-extrinsic-vs-intrinsic-sizing/script.js
@@ -3,3 +3,10 @@
 /* Constants for awesome and intrinsicSwitch */
 const awesome = document.querySelector('[data-element="awesome"]');
 const intrinsicSwitch = document.querySelector("#intrinsic-switch");
+
+intrinsicSwitch.addEventListener("change", () => {
+  awesome.setAttribute(
+    "data-sizing",
+    intrinsicSwitch.checked ? "intrinsic" : "extrinsic"
+  );
+});
