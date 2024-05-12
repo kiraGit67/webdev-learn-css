ed343584f0d76abbe1b1b3424b202871ec03a808
kiraGit67 <kirsten@sais.de>
2024-05-12 23:08:28 +0200

    adds constants for awesome and intrinsicSwitch

---
+++ 01-box-model/01-01-extrinsic-vs-intrinsic-sizing/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+/* Constants for awesome and intrinsicSwitch */
+const awesome = document.querySelector('[data-element="awesome"]');
+const intrinsicSwitch = document.querySelector("#intrinsic-switch");
