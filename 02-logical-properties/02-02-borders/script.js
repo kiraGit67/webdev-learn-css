e5dd4be8710e24e64531810e7761d014dab9bcd0
kiraGit67 <kirsten@sais.de>
2024-05-20 21:59:48 +0200

    adds eventListener to toggleElement to toggle direction property of myElements between ltr and rtl

--- 02-logical-properties/02-02-borders/script.js
+++ 02-logical-properties/02-02-borders/script.js
@@ -2,3 +2,9 @@
 
 const toggleElement = document.querySelector("#toggle-element");
 const myElements = document.querySelectorAll(".my-element");
+
+toggleElement.addEventListener("change", (event) => {
+  myElements.forEach((myElement) => {
+    myElement.style.direction = event.target.checked ? "rtl" : "ltr";
+  });
+});
