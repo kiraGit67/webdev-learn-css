e90d216c9f3cef4ce53a8af2b38f440c1f7f8ecf
kiraGit67 <kirsten@sais.de>
2024-05-20 18:48:55 +0200

    feat: adds const sampleMarginBlockStart and extends selectElement.addEventListener with sampleMarginBlockStart.style.setProperty functionality

--- 02-logical-properties/02-01-flow-relative-margin/script.js
+++ 02-logical-properties/02-01-flow-relative-margin/script.js
@@ -4,6 +4,9 @@ const toggleElement = document.querySelector("#toggle-element");
 const selectElement = document.querySelector("#select-writing-mode");
 
 const sample = document.querySelector(".sample");
+const sampleMarginBlockStart = document.querySelector(
+  "#sample--margin-block-start > p"
+);
 
 toggleElement.addEventListener("change", (event) => {
   sample.style.writingMode = event.target.checked ? "vertical-rl" : "initial";
@@ -11,4 +14,12 @@ toggleElement.addEventListener("change", (event) => {
 
 selectElement.addEventListener("change", (event) => {
   sample.style.writingMode = event.target.value;
+
+  if (event.target.value === "vertical-lr") {
+    sampleMarginBlockStart.style.setProperty("margin-block-end", "1rem");
+    sampleMarginBlockStart.style.setProperty("margin-block-start", "0");
+  } else {
+    sampleMarginBlockStart.style.setProperty("margin-block-start", "1rem");
+    sampleMarginBlockStart.style.setProperty("margin-block-end", "0");
+  }
 });
