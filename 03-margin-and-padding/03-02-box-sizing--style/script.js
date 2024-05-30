46efb261885379176787a9b09d7ce0a28e71770c
kiraGit67 <kirsten@sais.de>
2024-05-30 18:45:18 +0200

    changes myElement.setAttribute("data-size") to myElement.style.boxSizing

--- 03-margin-and-padding/03-02-box-sizing--style/script.js
+++ 03-margin-and-padding/03-02-box-sizing--style/script.js
@@ -2,8 +2,7 @@ const sizeSwitcher = document.querySelector("#size-switcher");
 const myElement = document.querySelector(".my-element");
 
 sizeSwitcher.addEventListener("change", () => {
-  myElement.setAttribute(
-    "data-size",
-    sizeSwitcher.checked ? "content-box" : "border-box"
-  );
+  myElement.style.boxSizing = sizeSwitcher.checked
+    ? "content-box"
+    : "border-box";
 });
