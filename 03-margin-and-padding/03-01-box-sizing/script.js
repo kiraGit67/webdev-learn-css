6b119f7fa85454fe0e3a06575b44e8c1e43a2133
kiraGit67 <kirsten@sais.de>
2024-05-30 18:27:38 +0200

    adds eventListener to sizeSwitcher to toggle between content-box and border-box

--- 03-margin-and-padding/03-01-box-sizing/script.js
+++ 03-margin-and-padding/03-01-box-sizing/script.js
@@ -1,2 +1,9 @@
 const sizeSwitcher = document.querySelector("#size-switcher");
 const myElement = document.querySelector(".my-element");
+
+sizeSwitcher.addEventListener("change", () => {
+  myElement.setAttribute(
+    "data-size",
+    sizeSwitcher.checked ? "content-box" : "border-box"
+  );
+});
