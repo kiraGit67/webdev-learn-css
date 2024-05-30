b5abb43622bffdc5bf48c05d12eb6c7d23d634c6
kiraGit67 <kirsten@sais.de>
2024-05-30 21:56:52 +0200

    adds constant for root element

--- 03-margin-and-padding/03-03-box-sizing--property/script.js
+++ 03-margin-and-padding/03-03-box-sizing--property/script.js
@@ -1,8 +1,5 @@
 const sizeSwitcher = document.querySelector("#size-switcher");
 const myElement = document.querySelector(".my-element");
+const root = document.documentElement;
 
-sizeSwitcher.addEventListener("change", () => {
-  myElement.style.boxSizing = sizeSwitcher.checked
-    ? "content-box"
-    : "border-box";
-});
+sizeSwitcher.addEventListener("change", () => {});
