f8889c8ae101d3f88b6d7fab0b7c0ab70ca4c9d5
kiraGit67 <kirsten@sais.de>
2024-05-30 22:04:59 +0200

    replaces myElement.style.boxSizing by root.style.setProperty

--- 03-margin-and-padding/03-03-box-sizing--property/script.js
+++ 03-margin-and-padding/03-03-box-sizing--property/script.js
@@ -2,4 +2,9 @@ const sizeSwitcher = document.querySelector("#size-switcher");
 const myElement = document.querySelector(".my-element");
 const root = document.documentElement;
 
-sizeSwitcher.addEventListener("change", () => {});
+sizeSwitcher.addEventListener("change", () => {
+  root.style.setProperty(
+    "--data-sizing",
+    sizeSwitcher.checked ? "content-box" : "border-box"
+  );
+});
