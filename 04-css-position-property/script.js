61ab97c44cdf7ed41a0378eb2a1655c402b454bb
kiraGit67 <kirsten@sais.de>
2024-05-31 19:10:00 +0200

    adds eventListener to selectPosition to change boxElement's style.position

--- 04-css-position-property/script.js
+++ 04-css-position-property/script.js
@@ -1,3 +1,7 @@
 const selectPosition = document.querySelector("#select-position");
 const boxElement = document.querySelector(".box");
 const rootElement = document.documentElement;
+
+selectPosition.addEventListener("change", () => {
+  boxElement.style.position = selectPosition.value;
+});
