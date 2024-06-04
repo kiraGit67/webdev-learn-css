1c58c58ba1b3495e0f7dfe15304ff88df8907290
kiraGit67 <kirsten@sais.de>
2024-06-04 19:15:12 +0200

    adds functionality to change .border-element's border-image-repeat

--- 07-borders/07-01-border-image-repeat/script.js
+++ 07-borders/07-01-border-image-repeat/script.js
@@ -1,3 +1,7 @@
 /** Set constants for select and .border-element */
 const borderImageRepeat = document.querySelector("#border-image-repeat");
 const borderElement = document.querySelector(".border-element");
+
+borderImageRepeat.addEventListener("change", () => {
+  borderElement.style.borderImageRepeat = `${borderImageRepeat.value} ${borderImageRepeat.value}`;
+});
