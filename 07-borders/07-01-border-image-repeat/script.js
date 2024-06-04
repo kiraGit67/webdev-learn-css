541a667b9cd05f614d73d9b67dd2b93d885f7ba3
kiraGit67 <kirsten@sais.de>
2024-06-04 19:11:14 +0200

    set constants for select and .border-element

---
+++ 07-borders/07-01-border-image-repeat/script.js
@@ -0,0 +1,3 @@
+/** Set constants for select and .border-element */
+const borderImageRepeat = document.querySelector("#border-image-repeat");
+const borderElement = document.querySelector(".border-element");
