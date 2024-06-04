ad1277c394132e5fe3a0b3d3b3134d06390b8632
kiraGit67 <kirsten@sais.de>
2024-06-04 19:40:13 +0200

    feat: replaces borderElement.style.borderImageRepeat and borderImageElement.style.borderImageRepeat by rootElement.style.setProperty

--- 07-borders/07-01-border-image-repeat/script.js
+++ 07-borders/07-01-border-image-repeat/script.js
@@ -5,6 +5,10 @@ const borderImageElement = document.querySelector(".border-image-element");
 const rootElement = document.documentElement;
 
 borderImageRepeat.addEventListener("change", () => {
-  borderElement.style.borderImageRepeat = `${borderImageRepeat.value} ${borderImageRepeat.value}`;
-  borderImageElement.style.borderImageRepeat = borderImageRepeat.value;
+  //borderElement.style.borderImageRepeat = `${borderImageRepeat.value} ${borderImageRepeat.value}`;
+  //borderImageElement.style.borderImageRepeat = borderImageRepeat.value;
+  rootElement.style.setProperty(
+    "--border-image-repeat-value",
+    borderImageRepeat.value
+  );
 });
