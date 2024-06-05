6eb2d98c1eb0d6e4a876e2564ba739d00075e602
kiraGit67 <kirsten@sais.de>
2024-06-05 19:42:02 +0200

    adds Variables for #result and .box-shadow-element

--- 08-shadows/08-01-box-shadow-playground/script.js
+++ 08-shadows/08-01-box-shadow-playground/script.js
@@ -5,3 +5,7 @@ const blur = document.querySelector("#blur");
 const spread = document.querySelector("#spread");
 const color = document.querySelector("#color");
 const inset = document.querySelector("#inset");
+
+/* Variables for #result and .box-shadow-element */
+const resultElement = document.querySelector("#result");
+const boxShadowElement = document.querySelector(".box-shadow-element");
