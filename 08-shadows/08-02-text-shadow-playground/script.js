04ece162d99770a5bafc7aaf7bd09ddccc81e71d
kiraGit67 <kirsten@sais.de>
2024-06-06 19:41:52 +0200

    adds variables for #result and .text-shadow-element

--- 08-shadows/08-02-text-shadow-playground/script.js
+++ 08-shadows/08-02-text-shadow-playground/script.js
@@ -4,4 +4,11 @@ const offsetY = document.querySelector("#offsetY");
 const blur = document.querySelector("#blur");
 const color = document.querySelector("#color");
 
-/*  */
+/* Variables for #result .text-shadow-element */
+const resultElement = document.querySelector("#result");
+const textShadowElement = document.querySelector(".text-shadow-element");
+
+/* adds function to change the inputs' values and text shadow stylings */
+const rendering = () => {
+  const result = `text-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${color.value}`;
+};
