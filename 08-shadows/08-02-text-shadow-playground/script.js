90ea1f7d276c8981648cbfce510500372576f51e
kiraGit67 <kirsten@sais.de>
2024-06-06 19:36:56 +0200

    adds variables for all inputs

---
+++ 08-shadows/08-02-text-shadow-playground/script.js
@@ -0,0 +1,7 @@
+/* Variables for inputs */
+const offsetX = document.querySelector("#offsetX");
+const offsetY = document.querySelector("#offsetY");
+const blur = document.querySelector("#blur");
+const color = document.querySelector("#color");
+
+/*  */
