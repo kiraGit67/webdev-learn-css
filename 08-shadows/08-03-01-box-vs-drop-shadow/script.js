339f1ab80a76cc9b59fc86fbd09a1a5ec0141079
kiraGit67 <kirsten@sais.de>
2024-06-08 20:36:46 +0200

    adds eventListener to toggleShadowElement to toggle between box and drop shadow style for figureImg

--- 08-shadows/08-03-01-box-vs-drop-shadow/script.js
+++ 08-shadows/08-03-01-box-vs-drop-shadow/script.js
@@ -1,2 +1,11 @@
 const toggleShadowElement = document.querySelector("#toggle-shadow");
 const figureImg = document.querySelector("figure img");
+
+toggleShadowElement.addEventListener("change", () => {
+  figureImg.setAttribute(
+    "style",
+    toggleShadowElement.checked
+      ? "filter: drop-shadow(0px 0px 10px rgba(0 0 0 / 30%))"
+      : "box-shadow : 0px 0px 10px rgba(0 0 0 / 30%)"
+  );
+});
