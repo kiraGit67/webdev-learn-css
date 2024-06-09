a9425cabafbc264c5f8f2ab7a3f9ae5c66bfc965
kiraGit67 <kirsten@sais.de>
2024-06-09 22:24:42 +0200

    adds eventListener to toggleElement for toggling demoBox.style.position between relative and static

--- 09-z-index/09-01-z-index-and-relative-positioning/script.js
+++ 09-z-index/09-01-z-index-and-relative-positioning/script.js
@@ -1,2 +1,9 @@
 const toggleElement = document.querySelector("#toggle-element");
 const demoBoxes = document.querySelectorAll(".demo-box");
+
+toggleElement.addEventListener("change", ({ target }) => {
+  demoBoxes.forEach(
+    (demoBox) =>
+      (demoBox.style.position = target.checked ? "relative" : "static")
+  );
+});
