83168e955a26575c98df49cd096fb70fcd128ccd
kiraGit67 <kirsten@sais.de>
2024-06-11 19:36:33 +0200

    feat: changes myElement.style.position = target.checked ? "relative" : "static"; myElement.style.zIndex = target.checked ? 0 : "auto"; to myElement.setAttribute(     "style",     target.checked ? "position: relative; z-index: 0;" : ""   );

--- 09-z-index/09-02-negative-z-index/script.js
+++ 09-z-index/09-02-negative-z-index/script.js
@@ -3,8 +3,13 @@ const myElement = document.querySelector(".my-element");
 const childElement = document.querySelector(".my-element>.child");
 
 toggleElement.addEventListener("change", ({ target }) => {
-  myElement.style.position = target.checked ? "relative" : "static";
-  myElement.style.zIndex = target.checked ? 0 : "auto";
+  //myElement.style.position = target.checked ? "relative" : "static";
+  //myElement.style.zIndex = target.checked ? 0 : "auto";
+  myElement.setAttribute(
+    "style",
+    target.checked ? "position: relative; z-index: 0;" : ""
+  );
+
   childElement.innerHTML = target.checked
     ? "I am on top of my parent."
     : "I am behind my parent.";
