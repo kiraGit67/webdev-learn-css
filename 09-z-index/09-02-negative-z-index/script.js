22dee480b801a80bbffe2a8589fcb24e4b33fe1d
kiraGit67 <kirsten@sais.de>
2024-06-11 19:30:53 +0200

    adds eventListener to toggleElement to toggle my.element's position between "relative" and "static" and its z-index between 0 and "auto"

--- 09-z-index/09-02-negative-z-index/script.js
+++ 09-z-index/09-02-negative-z-index/script.js
@@ -1,3 +1,11 @@
 const toggleElement = document.querySelector("#toggle-element");
 const myElement = document.querySelector(".my-element");
 const childElement = document.querySelector(".my-element>.child");
+
+toggleElement.addEventListener("change", ({ target }) => {
+  myElement.style.position = target.checked ? "relative" : "static";
+  myElement.style.zIndex = target.checked ? 0 : "auto";
+  childElement.innerHTML = target.checked
+    ? "I am on top of my parent."
+    : "I am behind my parent.";
+});
