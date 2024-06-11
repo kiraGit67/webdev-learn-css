13ac32cc6f9efcd67a27df51cf334b0760dae60b
kiraGit67 <kirsten@sais.de>
2024-06-11 19:08:10 +0200

    adds constants for toggleElement, myElement and childElement

---
+++ 09-z-index/09-02-negative-z-index/script.js
@@ -0,0 +1,3 @@
+const toggleElement = document.querySelector("#toggle-element");
+const myElement = document.querySelector(".my-element");
+const childElement = document.querySelector(".my-element>.child");
