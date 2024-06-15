23b2fb838e853943d9d2fea30f6f41dfa43ce2a3
kiraGit67 <kirsten@sais.de>
2024-06-15 17:48:09 +0200

    adds eventListener to marginInput to change calcElement's width and text

--- 11-functions/11-01-calc()/script.js
+++ 11-functions/11-01-calc()/script.js
@@ -1,3 +1,9 @@
 /* constants for input#margin, .calc-element */
 const marginInput = document.querySelector("#margin");
 const calcElement = document.querySelector(".calc-element");
+
+/* Function to change the inline margin of calcElement */
+marginInput.addEventListener("input", () => {
+  calcElement.style.width = `calc(100% - ${marginInput.value}rem)`;
+  calcElement.innerHTML = `I have a width of <code>calc(100% - ${marginInput.value}rem)</code>. I will fill all of my parent space except for <code>${marginInput.value}rem</code>.`;
+});
