0a15d2f4c9d73a808a952dcd08e69ee2b2d4c44e
kiraGit67 <kirsten@sais.de>
2024-06-17 13:23:28 +0200

    adds new folder 11-functions/11-03-transformations/11-03-011-rotation3d/

---
+++ 11-functions/11-03-transformations/11-03-011-rotation3d/script.js
@@ -0,0 +1,15 @@
+/* constants for input:range and .boxes > * */
+const rangeInput = document.querySelector("input[type='range']");
+const boxes = document.querySelectorAll("[data-transform]");
+
+/* EventListener for rangeInput to rotate the boxes */
+rangeInput.addEventListener("input", () => {
+  boxes.forEach((box) => {
+    box.setAttribute(
+      "style",
+      `transform: rotate${box.getAttribute("data-transform")}(${
+        rangeInput.value
+      }deg)`
+    );
+  });
+});
