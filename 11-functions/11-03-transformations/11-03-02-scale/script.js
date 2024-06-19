70f8f9018c1d537554b933547b034fdcef829038
kiraGit67 <kirsten@sais.de>
2024-06-19 20:23:18 +0200

    eventListener for scaleInput to scale the boxes

--- 11-functions/11-03-transformations/11-03-02-scale/script.js
+++ 11-functions/11-03-transformations/11-03-02-scale/script.js
@@ -1,3 +1,15 @@
 /* constants for .boxes>* and input:range */
 const scaleInput = document.querySelector("input[type='range']");
 const boxes = document.querySelectorAll("[data-transform]");
+
+/* eventListener for scaleInput to scale the boxes */
+scaleInput.addEventListener("input", () => {
+  boxes.forEach((box) => {
+    box.setAttribute(
+      "style",
+      `transform: scale${box.getAttribute("data-transform")}(${
+        scaleInput.value
+      })`
+    );
+  });
+});
