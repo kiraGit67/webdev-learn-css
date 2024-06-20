9da0bf06d0f7e614a8a257101368fbeab4eb2e79
kiraGit67 <kirsten@sais.de>
2024-06-20 19:56:15 +0200

    adds constants to .demo and input[type="range"]

---
+++ 11-functions/11-03-transformations/11-03-03-translate/script.js
@@ -0,0 +1,5 @@
+/* constants for .demo and input:range */
+const demoBox = document.querySelector(".demo");
+const translators = [...document.querySelectorAll("input[type='range']")];
+
+console.log(translators);
