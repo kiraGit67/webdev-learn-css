10219630bb31c6f93c87e3e456ca746f719c7631
kiraGit67 <kirsten@sais.de>
2024-06-20 20:07:48 +0200

    adds function update()

--- 11-functions/11-03-transformations/11-03-03-translate/script.js
+++ 11-functions/11-03-transformations/11-03-03-translate/script.js
@@ -3,3 +3,16 @@ const demoBox = document.querySelector(".demo");
 const translators = [...document.querySelectorAll("input[type='range']")];
 
 console.log(translators);
+
+/* eventListener for translators to translate the demoBox */
+function update() {
+  let transform = translators
+    .map((translator) => {
+      return `translate${translator.getAttribute("data-transform")}(${
+        translator.value
+      }px)`;
+    })
+    .join(" ");
+
+  demoBox.setAttribute("style", `transform: ${transform}`);
+}
