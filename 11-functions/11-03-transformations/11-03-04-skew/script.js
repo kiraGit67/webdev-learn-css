8c8d62941326a93c5d0b5d65bb6c47060528bf3a
kiraGit67 <kirsten@sais.de>
2024-06-25 21:58:18 +0200

    adds update() function and eventListener to each slider to apply update() function to demoBox

--- 11-functions/11-03-transformations/11-03-04-skew/script.js
+++ 11-functions/11-03-transformations/11-03-04-skew/script.js
@@ -1,3 +1,21 @@
 /* constants for .demo and skewInputs */
 const demoBox = document.querySelector(".demo");
-const sliders = document.querySelectorAll("input[type='range']");
+const sliders = [...document.querySelectorAll("input[type='range']")];
+
+/* update function for each slider */
+function update() {
+  let skewing = sliders
+    .map((slider) => {
+      return `skew${slider.getAttribute("data-transform")}(${slider.value}deg)`;
+    })
+    .join(" ");
+
+  demoBox.setAttribute("style", `transform: ${skewing}`);
+}
+
+/* eventListener for sliders */
+sliders.forEach((slider) =>
+  slider.addEventListener("input", () => {
+    update();
+  })
+);
