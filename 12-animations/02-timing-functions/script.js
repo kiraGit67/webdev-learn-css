bfba615852f4c566a50c0e827f611f401e8244a1
kiraGit67 <kirsten@sais.de>
2024-06-26 20:12:13 +0200

    adds eventListener to select#timing-choice to change timing-function for expander

--- 12-animations/02-timing-functions/script.js
+++ 12-animations/02-timing-functions/script.js
@@ -1,3 +1,7 @@
 /* constants for .expander and select */
 const expander = document.querySelector(".expander");
 const timingChoice = document.querySelector("#timing-choice");
+
+timingChoice.addEventListener("change", ({ target }) => {
+  expander.style.animationTimingFunction = target.value;
+});
