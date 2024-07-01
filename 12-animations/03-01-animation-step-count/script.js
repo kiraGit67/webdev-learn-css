109c1d70b081d98097c50c1df3560137dad23017
kiraGit67 <kirsten@sais.de>
2024-07-01 19:50:30 +0200

    adds constant for #step-count -> stepCounter

--- 12-animations/03-01-animation-step-count/script.js
+++ 12-animations/03-01-animation-step-count/script.js
@@ -1,6 +1,7 @@
 /* constants for toggle and demo element */
 const stepDemo = document.querySelector(".demo");
 const stepToggler = document.querySelector("#toggle-element");
+const stepCounter = document.querySelector("#step-count");
 
 /* eventListener for stepToggler to turn on step timing function */
 stepToggler.addEventListener("change", ({ target }) => {
