3b7b57a87f0652df9676ae1478ad34648ef41116
kiraGit67 <kirsten@sais.de>
2024-07-01 20:02:03 +0200

    adds constants for .callout and calloutText

--- 12-animations/03-01-animation-step-count/script.js
+++ 12-animations/03-01-animation-step-count/script.js
@@ -2,6 +2,10 @@
 const stepDemo = document.querySelector(".demo");
 const stepToggler = document.querySelector("#toggle-element");
 const stepCounter = document.querySelector("#step-count");
+const callout = document.querySelector(".callout");
+const calloutText = `<p>Switch the toggle on to enable the <code>steps</code> timing
+function.<br />Notice how even though there are only two keyframes, ${stepCounter.value} steps are created which
+results in staggered effect.</p>`;
 
 /* eventListener for stepToggler to turn on step timing function */
 stepToggler.addEventListener("change", ({ target }) => {
