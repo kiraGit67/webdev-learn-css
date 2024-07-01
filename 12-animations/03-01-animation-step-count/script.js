75c8dd923cdb0e4e594de1b277b636422df702f3
kiraGit67 <kirsten@sais.de>
2024-07-01 20:08:31 +0200

    adds eventListener to stepCounter to change calloutText

--- 12-animations/03-01-animation-step-count/script.js
+++ 12-animations/03-01-animation-step-count/script.js
@@ -3,9 +3,15 @@ const stepDemo = document.querySelector(".demo");
 const stepToggler = document.querySelector("#toggle-element");
 const stepCounter = document.querySelector("#step-count");
 const callout = document.querySelector(".callout");
-const calloutText = `<p>Switch the toggle on to enable the <code>steps</code> timing
-function.<br />Notice how even though there are only two keyframes, ${stepCounter.value} steps are created which
+
+/* eventListener for stepCounter to change count of steps */
+stepCounter.addEventListener("input", ({ target }) => {
+  let calloutText = `<p>Switch the toggle on to enable the <code>steps</code> timing
+function.<br />Notice how even though there are only two keyframes, ${target.value} steps are created which
 results in staggered effect.</p>`;
+
+  callout.innerHTML = calloutText;
+});
 
 /* eventListener for stepToggler to turn on step timing function */
 stepToggler.addEventListener("change", ({ target }) => {
