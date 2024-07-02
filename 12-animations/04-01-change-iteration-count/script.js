0a8944886ef74e6ce6e1ef2cc7ab40cb7c607a9b
kiraGit67 <kirsten@sais.de>
2024-07-02 20:24:06 +0200

    adds constants for figure.callout and calloutText, extends eventListener for changeIterationCount to change the callout text

--- 12-animations/04-01-change-iteration-count/script.js
+++ 12-animations/04-01-change-iteration-count/script.js
@@ -5,9 +5,15 @@ const restartButton = document.querySelector("button");
 /* constants for #change-iteration-count and :root element */
 const changeIterationCount = document.querySelector("#change-iteration-count");
 const rootElement = document.documentElement;
+const callout = document.querySelector("figure.callout");
 
 /* eventListener for changeIterationCount to change animation-iteration-count */
 changeIterationCount.addEventListener("input", ({ target }) => {
+  let calloutText = `<p>Press the <code>RESTART button</code> to restart the Animation after running within
+<code>animation-iteration-count: ${target.value}</code>.
+</p>`;
+  callout.innerHTML = calloutText;
+
   rootElement.style.setProperty("--iteration-count", target.value);
 });
 
