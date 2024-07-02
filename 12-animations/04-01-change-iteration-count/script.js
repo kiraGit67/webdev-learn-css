3904a2860cee5c71672c5eb5f997c30a61b6b41a
kiraGit67 <kirsten@sais.de>
2024-07-02 20:13:59 +0200

    adds constants for #change-iteration-count and :root element

--- 12-animations/04-01-change-iteration-count/script.js
+++ 12-animations/04-01-change-iteration-count/script.js
@@ -2,6 +2,10 @@
 const growerElement = document.querySelector(".grower");
 const restartButton = document.querySelector("button");
 
+/* constants for #change-iteration-count and :root element */
+const changeIterationCount = document.querySelector("#change-iteration-count");
+const rootElement = document.documentElement;
+
 /* eventListener for button to restart .grower.animate animation */
 restartButton.addEventListener("click", () => {
   growerElement.classList.remove("animate");
