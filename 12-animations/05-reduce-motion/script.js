c5afb7849f9a3eaffc46e99f0c7d1677201373e2
kiraGit67 <kirsten@sais.de>
2024-07-05 19:43:38 +0200

    adds eventListener to animationCheckbox to toggle reduced-motion of containerElement

--- 12-animations/05-reduce-motion/script.js
+++ 12-animations/05-reduce-motion/script.js
@@ -1,3 +1,12 @@
 /* constants for .container.reduced-motion and checkbox */
 const animationCheckbox = document.querySelector("input[type='checkbox']");
 const containerElement = document.querySelector(".container");
+
+/* eventListener to checkbox to reduce-motion of containerElement */
+animationCheckbox.addEventListener("change", () => {
+  if (animationCheckbox.checked) {
+    containerElement.classList.add("reduced-motion");
+  } else {
+    containerElement.classList.remove("reduced-motion");
+  }
+});
