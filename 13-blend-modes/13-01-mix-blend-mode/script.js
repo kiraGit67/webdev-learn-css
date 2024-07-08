917c24daded39bf0028a5bb97ee7912e6b8c1274
kiraGit67 <kirsten@sais.de>
2024-07-08 19:44:30 +0200

    adds constants for #toggle-element and .demo > img

---
+++ 13-blend-modes/13-01-mix-blend-mode/script.js
@@ -0,0 +1,3 @@
+/* adds constants for #toggle-element and .demo > img */
+const toggleElement = document.querySelector("input[type='checkbox']");
+const demoImg = document.querySelector(".demo>img");
