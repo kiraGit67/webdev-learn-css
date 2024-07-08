c361df46e0c3c9b5612cfa5e2f1fe8535d1002a5
kiraGit67 <kirsten@sais.de>
2024-07-08 20:00:57 +0200

    adds constant for input[type="range"]

--- 13-blend-modes/13-01-mix-blend-mode/script.js
+++ 13-blend-modes/13-01-mix-blend-mode/script.js
@@ -1,6 +1,7 @@
 /* adds constants for #toggle-element and .demo > img */
 const toggleElement = document.querySelector("input[type='checkbox']");
 const demoImg = document.querySelector(".demo>img");
+const rangeElement = document.querySelector("input[type='range']");
 
 toggleElement.addEventListener("change", ({ target }) => {
   demoImg.style.mixBlendMode = target.checked ? "multiply" : "normal";
