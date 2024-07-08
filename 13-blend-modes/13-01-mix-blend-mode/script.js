7d03087b1e56c2729f9ff8ab019c592e694807f1
kiraGit67 <kirsten@sais.de>
2024-07-08 19:47:04 +0200

    adds eventListener to toggleElement to toggle mix-blend-mode from normal to multiply

--- 13-blend-modes/13-01-mix-blend-mode/script.js
+++ 13-blend-modes/13-01-mix-blend-mode/script.js
@@ -1,3 +1,7 @@
 /* adds constants for #toggle-element and .demo > img */
 const toggleElement = document.querySelector("input[type='checkbox']");
 const demoImg = document.querySelector(".demo>img");
+
+toggleElement.addEventListener("change", ({ target }) => {
+  demoImg.style.mixBlendMode = target.checked ? "multiply" : "normal";
+});
