ae2a93ed93a8d48d4a0b068801a3536a5d05159c
kiraGit67 <kirsten@sais.de>
2024-07-09 10:32:51 +0200

    adds 13-blend-modes/13-01-01-select-blend-mix-mode/script.js

---
+++ 13-blend-modes/13-01-01-select-mix-blend-mode/script.js
@@ -0,0 +1,12 @@
+/* adds constants for #toggle-element and .demo > img */
+const toggleElement = document.querySelector("input[type='checkbox']");
+const demoImg = document.querySelector(".demo>img");
+const rangeElement = document.querySelector("input[type='range']");
+
+toggleElement.addEventListener("change", ({ target }) => {
+  demoImg.style.mixBlendMode = target.checked ? "multiply" : "normal";
+});
+
+rangeElement.addEventListener("input", ({ target }) => {
+  demoImg.style.filter = `brightness(${target.value}%)`;
+});
