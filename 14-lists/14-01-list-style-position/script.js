474caddd43971e05c835e7e0a61edbc480ef7f3f
kiraGit67 <kirsten@sais.de>
2024-07-30 19:52:21 +0200

    adds eventListener for toggleElement to toggle .inside class

--- 14-lists/14-01-list-style-position/script.js
+++ 14-lists/14-01-list-style-position/script.js
@@ -1,3 +1,8 @@
 /* adds constants for toggle element and lists */
 const toggleElement = document.querySelector("#toggle-element");
 const lists = [...document.querySelectorAll("ul")];
+
+/* eventListener for toggleElement to toggle .inside class */
+toggleElement.addEventListener("change", () => {
+  lists.forEach((list) => list.classList.toggle("inside"));
+});
